e08732801e624b83fef7b2e1c18b9ca398129400
olekak9 <olek.ak9@gmail.com>
2025-03-18 10:34:03 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/globals.css";
+import "../styles/globals.css";
+
 
 export default function Home() {
   const [clients, setClients] = useState([
